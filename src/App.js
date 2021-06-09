2284245df6b555d1fc53b07489c4792e6838bdaa
omunoz <steven.120189@gmail.com>
2021-06-08 20:43:14 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 // Imports
-import React, { useEffect, useState } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
-// import setAuthToken from './utils/setAuthToken';
+import React, { useEffect, useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 // CSS
-import './App.css';
+import "./App.css";
 // Components
-import Welcome from './components/Welcome';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Profile from './components/Profile';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import About from './components/About';
+import Welcome from "./components/Welcome";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Profile from "./components/Profile";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import About from "./components/About";
+
+//Private route component
+const PrivateRoute = ({component: Component, ...rest}) => {
+  console.log('This is a private route')
+  let user = localStorage.getItem('jwtToken')
+  return <Route {...rest} render={ (props) => {
+    return user ? <Component {...rest} {...props} /> : <Redirect to='/login' />
+  }} />
+}
+
 
 function App() {
   // Set state values
 
+  const [currentUser, setCurrentUser] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  
+  useEffect(() => {
+    let token;
+    //if there is token inside of local storage, then the user is not authenticated
+    if (!localStorage.getItem("jwtToken")) {
+      console.log("is not authenticated");
+      setIsAuthenticated(false);
+    } else {
+      token = jwt_decode(localStorage.getItem("jwtToken"));
+      console.log("token", token);
+      setAuthToken(token);
+      setCurrentUser(token);
+    }
+  }, []);
+  
+  const nowCurrentUser = (userData) => {
+    console.log("--- inside nowCurrentUser ---");
+    setCurrentUser(userData);
+    setIsAuthenticated(true)
+  };
+
+  const handleLogout = () => {
+
+    if(localStorage.getItem("jwtToken")) {    //determine if there is a jwt token
+      localStorage.removeItem('jwtToken')    //remove if thre is a jwt
+      setCurrentUser(null);     //set currentUser to null
+      setIsAuthenticated(false)     //set auth to false
+    }
+  }
+
   return (
     <div className="App">
-      {/* <Navbar  /> */}
-      
+      <Navbar isAuth={isAuthenticated} handleLogout={handleLogout} /> 
+      <div className='container mt-5'>
+        <Switch>
+          <Route path='/signup' component={Signup}></Route>
+          <Route path='/login' render={(props) => <Login {...props} user={currentUser} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} /> } />
+          <Route path='/about' component={About} />
+          <Route exact path='/' component={Welcome}/>
+          <PrivateRoute path='/profile' component={Profile} user={currentUser} handleLogout={handleLogout} />
+        </Switch>
+      </div>
+
       <Footer />
     </div>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -29,7 +29,54 @@ const Signup = () => {
 
 
 
-  const handleSubmit = (e) => {};
+//   const handleSubmit = (e) => {
+//       e.preventDefault();
+
+//       //check to make sure passwords match
+//       if(password === confirmPassword && password.length >= 8) {
+//           const payload = {name, email, password};
+//           let url = `${REACT_APP_SERVER_URL}/api/users/signup`
+//           axios.post(url, payload)
+//           .then(response => {
+//               console.log(response.data);
+//               setRedirect(true);
+//           })
+//           .catch(error => {
+//               console.log(error)
+//           })
+//         } else {
+//             if(!password === confirmPassword) {
+//                 alert('Password and Confirm password need to match. Please try again...')
+//             } else {
+//                 alert('Password needs to be at least 8 characters or more. Please try again...')
+//             }
+//         }
+//   };
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    // check to make sure passwords match
+    if (password === confirmPassword && password.length >= 8) {
+        const payload = { name, email, password };
+        let url = `${REACT_APP_SERVER_URL}/api/users/signup`;
+        try {
+            let response = await axios.post(url, payload);
+            let { data } = response;
+            console.log(data);
+            setRedirect(true);
+        } catch (error) {
+            alert('Error occurred, please try again...');
+        }
+    } else {
+        if (!password === confirmPassword) {
+            alert('Password and Confirm Password need to match. Please try again...');
+        } else {
+            alert('Password needs to be at least 8 characters or more. Please try again...');
+        }
+    }
+}
+
+if (redirect) return <Redirect to="/login" />
 
   return (
     <div className="row mt-4">
